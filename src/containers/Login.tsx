7b574d803ea4cf18c6dbf968118ef282d4b67edc
YangJongBin <toutopia0@gmail.com>
2022-01-13 23:35:05 +0900

    Set react native elements

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  Button,
+} from 'react-native';
+import {ThemeProvider} from 'react-native-elements';
 
 interface Props {}
+interface Theme {
+  Button: {
+    raised: boolean;
+  };
+}
+
+const theme: Theme = {
+  Button: {
+    raised: true,
+  },
+};
 
 export default function Login(props: Props) {
   return (
-    <View>
-      <Text>Login View</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <TextInput placeholder="ID" style={styles.textInput}></TextInput>
+        <TextInput placeholder="PW" style={styles.textInput}></TextInput>
+        <Button title="Sign In" loading={false} style={styles.button} />
+      </KeyboardAvoidingView>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'pink',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: 100,
+  },
+  textInput: {
+    width: '100%',
+    height: 30,
+    backgroundColor: 'gray',
+  },
+});
