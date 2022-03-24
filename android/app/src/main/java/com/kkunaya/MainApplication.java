faf38ccadd7f393a22b8910a0fb645729782d6db
YangJongBin <toutopia0@gmail.com>
2022-03-24 22:01:56 +0900

    firestore 연동

--- android/app/src/main/java/com/kkunaya/MainApplication.java
+++ android/app/src/main/java/com/kkunaya/MainApplication.java
@@ -14,6 +14,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Arrays;
 import com.horcrux.svg.SvgPackage; 
+import androidx.multidex.MultiDexApplication;
  
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
 import org.unimodules.adapters.react.ReactModuleRegistryProvider;
--- src/containers/UserSetting.tsx
+++ src/containers/UserSetting.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 interface Props {
     
 }
 
+interface User {
+  displayName: string;
+  email: string;
+}
+
+interface Params {
+  type: number;
+}
+
 const UserSetting = (props: Props) => {
+  const ref = firestore().collection('user');
+  const user: User = auth().currentUser;
+
+  async function insertUserInfo(params: Params) {
+    await ref.add({
+      displayName: user?.displayName,
+      email: user.email,
+      type: params.type,
+    });
+  }
+
   return (
     <SafeAreaView>
       <Button title="test" onPress={()=>{
-        const user = auth().currentUser;
-
         console.log(user);
 
-        auth().currentUser?.updateProfile({
-          type: 'soldier'
+        insertUserInfo({
+          type: 1
         });
       }}></Button>
     </SafeAreaView>
