8ea8117527e70affce20bfe8e2953165792aec6c
YangJongBin <toutopia0@gmail.com>
2022-07-05 21:46:00 +0900

    Detail 화면 레이아웃 세팅, react-spring 라이브러리 추가

--- src/components/DetailComponent.tsx
+++ src/components/DetailComponent.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {useChain} from '@react-spring/native';
+
 
 // TODO:
 interface Props {
@@ -13,10 +15,32 @@ interface Props {
 }
 
 const DetailComponent = (props: Props) => {
+  const [gaugeWidth, setGaugeWidth] = useState<number>(0);
+
+  const Gauge = () => {
+    return <View style={{flex:1, backgroundColor: 'gray', marginHorizontal: 10, marginBottom:10}}>
+      <View style={{width: '50%', height: gaugeWidth, backgroundColor: 'green'}}></View>
+    </View>;
+  };
 
   return (
     <View style={styles.container}>
-      <View style={[styles.card , {flex: 2}]}><Text></Text></View>
+      <View style={[styles.card , {flex: 2}]}>
+        <View style={{flexDirection: 'column', flex:1}}>
+          <View style={{flex: 4, flexDirection: 'row', padding: 5}}>
+            <View style={{flex:2, justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={styles.nickNameText}>양군인</Text>
+            </View>
+            <View style={{flex:3, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+              <View><Text style={styles.startEndDateText}>입대일     2022.07.05</Text></View>
+              <View><Text style={styles.startEndDateText}>전역일     2022.07.06</Text></View>
+            </View>
+          </View>
+          <View style={{flex: 3,backgroundColor: 'red'}}>
+            <Gauge></Gauge>
+          </View>
+        </View>
+      </View>
       <View style={styles.flexDirectionRow}>
         <View style={[styles.card, { flex: 1}]}></View>
         <View style={[styles.card, { flex: 1}]}></View>
@@ -50,5 +74,13 @@ const styles = StyleSheet.create({
       width: 0,
       height: 5
     }
-  }
+  },
+  nickNameText: {
+    fontWeight: 'bold',
+    fontSize:20
+  },
+  startEndDateText: {
+    fontWeight: 'bold',
+    margin: 5
+  },
 });
